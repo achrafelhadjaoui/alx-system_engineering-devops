88387e8f2a441d19038916067d17f20301412bf5
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2024-08-14 19:24:34 -0700

    using puppet

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# fixing Apache error and automate it using puppet.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
