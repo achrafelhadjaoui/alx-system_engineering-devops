d0497d44da2d10368a4f93b929c75d93e04e4740
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2024-12-29 23:12:42 +0100

    update task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,7 @@
-# Using Puppet, create a file in /tmp
-
-file { '/tmp/school':
-  ensure  => file,
+# create a file in /tmp
+file {'/tmp/school':
   mode    => '0744',
-  content => 'I love Puppet',
   owner   => 'www-data',
   group   => 'www-data',
+  content => 'I love Puppet'
 }
